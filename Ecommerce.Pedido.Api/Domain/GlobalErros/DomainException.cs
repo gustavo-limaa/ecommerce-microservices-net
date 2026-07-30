13e7f07852addb6fe4caa5bc77fa085cf1ed2051
gustavo-lima <zandergustavolara@gmail.com>
2026-07-29 22:43:54 -0400

    Feat(Domain):The entity modeling is complete. Now configure the database.

---
+++ Ecommerce.Pedido.Api/Domain/GlobalErros/DomainException.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.Pedido.Api.Domain.GlobalErros
+{
+    public class DomainException : Exception
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ Ecommerce.Pedido.Api/Domain/Values.Objects/ObjectEmail.cs
@@ -0,0 +1,34 @@
+﻿using Ecommerce.Pedido.Api.Domain.Common;
+using Ecommerce.Pedido.Api.Domain.GlobalErros;
+using System.Text.RegularExpressions;
+
+namespace Ecommerce.Pedido.Api.Domain.Values.Objects;
+
+public partial record ObjectEmail
+{
+    public string Valor { get; private init; }
+
+    [GeneratedRegex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", RegexOptions.IgnoreCase)]
+    private static partial Regex EmailRegex();
+
+    public ObjectEmail(string valor)
+    {
+        var (email, error) = Criar(valor);
+        if (email == null) throw new DomainException(error);
+        Valor = email.Valor;
+    }
+    private ObjectEmail()
+    {
+    }
+
+    private ObjectEmail(string valor, bool validado) => Valor = valor;
+
+    public static (ObjectEmail? Email, string Error) Criar(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input)) return (null, DomainMessages.EmailMSG.Obrigatorio);
+        var valorTratado = input.Trim().ToLower();
+        if (!EmailRegex().IsMatch(valorTratado)) return (null, DomainMessages.EmailMSG.FormatoInvalido);
+
+        return (new ObjectEmail(valorTratado, true), string.Empty);
+    }
+}
